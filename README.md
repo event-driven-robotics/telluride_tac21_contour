3b98160e527172987bbef899a674450b639155dd
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2021-05-26 12:20:54 +0200

    last state

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 Contour following with iCub
 ======================
 
+yarpserver --write
+
+yarpdev --device transformServer --ROS::enable_ros_publisher false --ROS::enable_ros_subscriber false
+
+gazebo $ROBOTOLOGY_SUPERBUILD_INSTALL_PREFIX/share/gazebo/worlds/vte_scenario.sdf
+
 yarprobotinterface --context gazeboCartesianControl --config no_legs.xml
 
 iKinCartesianSolver --context gazeboCartesianControl --part right_arm
+
+./contour_following
--- contour_following.cpp
+++ contour_following.cpp
@@ -26,6 +26,7 @@
 // Skin simulation libraries
 #include <gazebo/Skin.hh>
 #include <iCub/skinDynLib/skinContact.h>
+#include <iCub/iKin/iKinFwd.h>
 
 using namespace yarp::os;
 using namespace yarp::dev;
@@ -38,12 +39,14 @@ class ContourFollowingModule : public RFModule, public yarp::os::Thread
 private:
     BufferedPort<iCub::skinDynLib::skinContactList> port;
     PolyDriver drv_arm;
-    ICartesianControl *arm;
+    PolyDriver drv_finger;
+    ICartesianControl *cartControl;
 
     yarp::sig::Matrix R;
-    yarp::sig::Vector x0{-.25, .0, -.05}, o0; //TODO tune starting position
+    yarp::sig::Vector x0{-0.1, 0.0, 0.0}, o0; //TODO tune starting position
     static constexpr double wait_ping{.1};
     static constexpr double wait_tmo{3.};
+    double y_min, y_max, y_delta, y;
     std::string robot, which_arm;
     int startup_context_id_arm;
     double period;
@@ -69,7 +72,6 @@ private:
 public:
     bool configure(ResourceFinder &rf)
     {
-        std::cout << "SIMON YOU ARE BEAUTIFUL!" << std::endl;
 
         // Reading command-line parameters
         setName((rf.check("name", yarp::os::Value("/contour_following")).asString()).c_str());
@@ -86,16 +88,60 @@ public:
         {
             return false;
         }
-        drv_arm.view(arm);
 
-        arm->storeContext(&startup_context_id_arm); //Storing initial context for restoring it later
-        arm->setTrajTime(.6);                       // Each trajectory would take this much amount of time to perform
+        // drv_arm.view(cartControl);
+
+        // Property prop_encoders;
+        // prop_encoders.put("device", "remote_controlboard");
+        // prop_encoders.put("local", "/" + getName() + "/controlboard/" + which_arm);
+        // prop_encoders.put("remote", "/" + robot + "/" + which_arm);
+        // if (drv_finger.open(prop_encoders))
+        // {
+        //     /* Try to retrieve the view. */
+        //     if (!(drv_finger.view(cartControl) && cartControl != nullptr))
+        //     {
+        //         std::cout << "Could not view driver" << std::endl;
+
+        //         return false;
+        //     }
+        //     // /* Try to retrieve the control limits view. */
+        //     // if (!(drv_finger.view(ilimits_)) || (ilimits_ == nullptr))
+        //     //     throw std::runtime_error(log_name_ + "::ctor. Error: unable get view for finger control limits.");
+        // }
+        // else
+        // {
+        //     std::cout << "Could not open remote controlboard" << std::endl;
+        //     return false;
+        // }
+
+        IEncoders *iencs;
+
+        int nEncs;
+        iencs->getAxes(&nEncs);
+        Vector encs(nEncs);
+        iencs->getEncoders(encs.data());
+
+        Vector joints;
+        iCub::iKin::iCubFinger finger("right_index"); // relevant code to get the position of the finger tip
+        finger.getChainJoints(encs, joints);          // wrt the end-effector frame
+        Matrix tipFrame = finger.getH((M_PI / 180.0) * joints);
+
+        Vector tip_x = tipFrame.getCol(3);
+        Vector tip_o = yarp::math::dcm2axis(tipFrame);
+        cartControl->attachTipFrame(tip_x, tip_o); // establish the new controlled frame
+
+        cartControl->storeContext(&startup_context_id_arm); //Storing initial context for restoring it later
+        cartControl->setTrajTime(.6);                       // Each trajectory would take this much amount of time to perform
 
         // Enabling all degrees of freedom
         yarp::sig::Vector dof;
-        arm->getDOF(dof);
+        cartControl->getDOF(dof);
         dof = 1.;
-        arm->setDOF(dof, dof);
+        cartControl->setDOF(dof, dof);
+
+        yarp::sig::Vector curDof;
+        cartControl->getDOF(curDof);
+        std::cout << curDof.toString().c_str() << std::endl;
 
         // Moving to starting position
         R = yarp::math::zeros(3, 3);
@@ -105,9 +151,11 @@ public:
         R(1, 2) = -1.;
         o0 = yarp::math::dcm2axis(R);
 
-        arm->goToPoseSync(x0, o0);
-        arm->waitMotionDone(wait_ping, wait_tmo);
-        arm->setPosePriority("position");
+        cartControl->goToPoseSync(x0, o0);
+        cartControl->waitMotionDone(wait_ping, wait_tmo);
+        cartControl->setPosePriority("position");
+
+        y = y_max;
 
         // open and connect port for reading skin events
         port.open("/skin_events");
@@ -140,7 +188,42 @@ public:
     // Asynchronous update that runs in a separate thread as fast as it can
     void run()
     {
-        //TODO add example movement
+        yarp::sig::Vector Xwrist, Owrist;
+        cartControl->getPose(8, Xwrist, Owrist); // get position and orientation of the last joint (wrist yaw)
+
+        yarp::sig::Vector Xelbow, Oelbow;
+        cartControl->getPose(6, Xelbow, Oelbow); // get position and orientation of the last joint (wrist yaw)
+
+        double theta = -atan2(Xelbow[1] - Xwrist[1], Xelbow[0] - Xwrist[0]);
+
+        yarp::sig::Matrix Rtheta_y;
+        Rtheta_y = yarp::math::zeros(3, 3); // rotation matrix around y
+
+        Rtheta_y(0, 0) = cos(theta);
+        Rtheta_y(0, 1) = 0;
+        Rtheta_y(0, 2) = sin(theta);
+        Rtheta_y(1, 0) = 0;
+        Rtheta_y(1, 1) = 1;
+        Rtheta_y(1, 2) = 0;
+        Rtheta_y(2, 0) = -sin(theta);
+        Rtheta_y(2, 1) = 0;
+        Rtheta_y(2, 2) = cos(theta);
+
+        cartControl->goToPoseSync(x0 + yarp::sig::Vector{0., y, 0.}, yarp::math::dcm2axis(R * Rtheta_y));
+        cartControl->waitMotionDone(wait_ping, wait_tmo);
+
+        std::cout << y << std::endl;
+
+        yarp::sig::Vector xdhat, odhat, q_arm;
+        cartControl->getDesired(xdhat, odhat, q_arm);
+
+        yarp::sig::Vector xarm, oarm;
+        cartControl->getPose(xarm, oarm);
+
+        y -= y_delta;
+
+        // y >= -y_min - y_delta/2.;
+
         // PUT YOUR CODE HERE
     }
 
@@ -160,8 +243,8 @@ public:
     {
         yInfo() << "Closing the module...";
 
-        arm->stopControl();
-        arm->restoreContext(startup_context_id_arm);
+        cartControl->stopControl();
+        cartControl->restoreContext(startup_context_id_arm);
         drv_arm.close();
 
         yInfo() << "...done!";
